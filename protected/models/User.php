822160f0410c28b8ea22a1eeabef6160868501d4
Adam Gavin <aaadamaa@gmail.com>
2015-02-16 19:03:41 -0400

    Added database authentication to UserIdentity.php, Added validatePassword() and hashPassword() to User model.

--- protected/models/User.php
+++ protected/models/User.php
@@ -96,4 +96,14 @@ class User extends CActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
+
+	public function validatePassword($password)
+	{
+		return CPasswordHelper::verifyPassword($password, $this->password);
+	}
+
+	public function hashPassword($password)
+	{
+		return CPasswordHelper::hashPassword($password);
+	}
 }
