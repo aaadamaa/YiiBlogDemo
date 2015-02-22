23f19cbf41ea7f4e6d1576be91693f3abe6b1da1
Adam Gavin <aaadamaa@gmail.com>
2015-02-22 11:55:35 -0400

    copied Post's view from demo for formatting

--- protected/views/post/view.php
+++ protected/views/post/view.php
@@ -1,41 +1,23 @@
 <?php
-/* @var $this PostController */
-/* @var $model Post */
-
 $this->breadcrumbs=array(
-	'Posts'=>array('index'),
 	$model->title,
 );
-
-$this->menu=array(
-	array('label'=>'List Post', 'url'=>array('index')),
-	array('label'=>'Create Post', 'url'=>array('create')),
-	array('label'=>'Update Post', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Post', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Post', 'url'=>array('admin')),
-);
+$this->pageTitle=$model->title;
 ?>
 
-<h1>View Post #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
+<?php $this->renderPartial('_view', array(
 	'data'=>$model,
-	'attributes'=>array(
-		'title',
-		'content',
-		'tags',
-	),
 )); ?>
 
 <div id="comments">
-	<?php if($model->commentCount >= 1): ?>
+	<?php if($model->commentCount>=1): ?>
 		<h3>
-			<?php echo $model->commentCount . 'comments(s)'; ?>
+			<?php echo $model->commentCount>1 ? $model->commentCount . ' comments' : 'One comment'; ?>
 		</h3>
 
-		<?php $this->renderPartial('_comments', array(
+		<?php $this->renderPartial('_comments',array(
 			'post'=>$model,
-			'comments'=>$model->comments
+			'comments'=>$model->comments,
 		)); ?>
 	<?php endif; ?>
 
@@ -46,8 +28,9 @@ $this->menu=array(
 			<?php echo Yii::app()->user->getFlash('commentSubmitted'); ?>
 		</div>
 	<?php else: ?>
-		<?php $this->renderPartial('/comment/_form', array(
+		<?php $this->renderPartial('/comment/_form',array(
 			'model'=>$comment,
 		)); ?>
 	<?php endif; ?>
+
 </div><!-- comments -->
